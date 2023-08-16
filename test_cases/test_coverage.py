ff67cbf488d7e99a63fe8d873ea17de2947cd851
ssuxue <53949579+ssuxue@users.noreply.github.com>
2023-08-16 13:41:18 +0800

    Create test_coverage.py

---
+++ test_cases/test_coverage.py
@@ -0,0 +1,40 @@
+import pytest
+
+
+@pytest.mark.parametrize('x, y', [(2, 3), (7, 7)])
+def test_get(x, y):
+    res = 0
+    if x > 0 and y > 0:
+        res = x
+    return res
+
+
+def test_assert1():
+    assert 1 == 0
+
+
+def test_assert2():
+    assert 1 > 0
+
+
+@pytest.mark.parametrize('x, y', [(5, 2), (7, 7)])
+def test_post(x, y):
+    res = 0
+    match = True
+    if match:
+        res = 1 << 2
+    else:
+        res = 1 >> 2
+    return res
+
+
+@pytest.mark.parametrize('x, y', [(5, 2), (1, 0), (2, 3), (7, 7)])
+def test_del(x, y):
+    if x > y:
+        return 0
+    match = x > y
+    if match:
+        res = 0b0011 << 2
+    else:
+        res = 0b1101 >> 2
+    return res
