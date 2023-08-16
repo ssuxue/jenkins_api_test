7c0ffcf3b48b9d8c87d34d7dd8a3565d32a03d58
ssuxue <53949579+ssuxue@users.noreply.github.com>
2023-08-16 13:43:17 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,28 @@
+import coverage
+import pytest
+
+from utils.diff_parser import code_diff_map
+from utils.reporter import CustomCoverage
+
+
+def main():
+    m = code_diff_map('./log.diff')
+    cov = CustomCoverage()
+    # cov = coverage.Coverage()
+    cov.start()
+
+    pytest.main(['-v', '-q'])
+
+    cov.stop()
+    cov.save()
+
+    report = cov.report(show_missing=True)
+
+    for k, v in enumerate(report):
+        print(f'{k} - {v} --> {report[v]}')
+    cov.html_report()
+    print(m)
+
+
+if __name__ == '__main__':
+    main()
