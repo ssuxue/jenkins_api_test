b32953ad149cda44a654c6a7dc1f0863280502bd
ssuxue <53949579+ssuxue@users.noreply.github.com>
2023-08-18 17:10:40 +0800

    modify delta coverage rate

--- run.py
+++ run.py
@@ -28,8 +28,8 @@ def main():
 
     rate = cov.delta_coverage_rate(m)
     # report = cov.coverage_data(m)
-    delta_cov_rate = '{}%'.format(round(rate * 100, 0))
-    print('Incremental code coverage rate is {}'.format(delta_cov_rate))
+    delta_cov_rate = '{}'.format(round(rate * 100, 0))
+    print('Incremental code coverage rate is {}%'.format(delta_cov_rate))
     print(delta_cov_rate)
     cov.html_report()
 
