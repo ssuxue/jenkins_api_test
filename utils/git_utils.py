e38c777b0638f2cadece6c1801838c4994e5e402
ssuxue <53949579+ssuxue@users.noreply.github.com>
2023-10-17 17:36:45 +0800

    Update git_utils.py

--- utils/git_utils.py
+++ utils/git_utils.py
@@ -116,8 +116,8 @@ def rerun(api: GheRepo, rid: int, err_code: Union[int, str]):
     ids = []
 
     for job in jobs:
-        # if job["status"] == "completed" and job["conclusion"] == "failure":
-            # ids.append(job["id"])
+        if job["status"] == "completed" and job["conclusion"] == "failure":
+            ids.append(job["id"])
         ids.append(job["id"])
     print(ids)
 
