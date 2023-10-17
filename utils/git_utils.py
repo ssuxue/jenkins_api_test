e6261cf6fe82fc0f9525cd6370bdb490a016b7ca
ssuxue <53949579+ssuxue@users.noreply.github.com>
2023-10-17 12:13:29 +0800

    Update git_utils.py

--- utils/git_utils.py
+++ utils/git_utils.py
@@ -106,8 +106,9 @@ def rerun_failed_jobs(api: GheRepo, run_id: int):
 
 
 def rerun(api: GheRepo, rid: int, err_code: Union[int, str]):
-    # res = api.get(f"actions/runs/{rid}/jobs")
-    # res = json.dumps(res)
+    res = api.get(f"actions/runs/{rid}/jobs")
+    re = json.dumps(res)
+    print(re)
     # res = res.replace("'", '"')
     jobs = res["jobs"]
     ids = []
